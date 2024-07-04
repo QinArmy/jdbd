8e1eec1eecefe6df758710a4c7f3359ce529faf0
zoro <zoro@qinarmy.io>
2024-07-04 19:24:07 +0800

    modify

--- jdbd-spi/src/main/java/io/jdbd/util/EscapeMode.java
+++ jdbd-spi/src/main/java/io/jdbd/util/EscapeMode.java
@@ -1,33 +1,12 @@
 package io.jdbd.util;
 
-import io.jdbd.meta.BooleanMode;
 
 public enum EscapeMode {
-    DEFAULT(BooleanMode.TRUE),
+    DEFAULT,
 
-    BACK_SLASH(BooleanMode.TRUE),
+    BACK_SLASH,
 
-    UNICODE(BooleanMode.TRUE),
-
-    DOLLAR_QUOTED(BooleanMode.TRUE),
-
-    DEFAULT_NO_TYPE(BooleanMode.FALSE),
-
-    BACK_SLASH_NO_TYPE(BooleanMode.FALSE),
-
-    UNICODE_NO_TYPE(BooleanMode.FALSE),
-
-    DOLLAR_QUOTED_NO_TYPE(BooleanMode.FALSE),
-
-    ARRAY_ELEMENT(BooleanMode.UNKNOWN),
-
-    ARRAY_ELEMENT_PART(BooleanMode.UNKNOWN);
-
-    public final BooleanMode typeMode;
-
-    EscapeMode(BooleanMode typeMode) {
-        this.typeMode = typeMode;
-    }
+    UNICODE;
 
 
 }
